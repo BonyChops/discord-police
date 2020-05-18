1e57a528c24320e8aa009e81b9cbdac5f5af63f6
Bony_Chops <office.kentarou@gmail.com>
2020-05-18 11:26:31 +0900

    Edit

--- app.js
+++ app.js
@@ -261,70 +261,70 @@ client.on('ready', async() => {
 });
 
 client.on('message', async msg => {
-  googleTTS('Hello World', 'en', 1)
-    if(msg.author != client.user){
-      if(msg.channel.id == ids.logCh) {msg.delete(); return;}
-      if(msg.channel.id == ids.terminalCh){
-            if(msg.content == "mode disable"){
-              client.user.setPresence({
-                status: "dnd",  //You can show online, idle....
-                activity: {
-                    name: "☓この鯖は危険な状態です",  //The message shown
-                    type: "LISTENING" //PLAYING: WATCHING: LISTENING: STREAMING:
-                }
-              });
-              const embed = {
-                "title": "**リアルタイムスキャンは無効です**",
-                "description": `サーバーを保護するには、今すぐリアルタイムスキャンを有効にしてください。`,
-                "color": 16711680,
-                "timestamp": new Date(),
-                "footer": {
-                  "icon_url": "https://cdn.discordapp.com/avatars/709077937005264948/ebe1823c4fd5cd615d67915ba4c2d5a8.png",
-                  "text": "Protected by Bony SECURITY POLICE"
-                },
-                "thumbnail": {
-                  "url": "https://i.imgur.com/3wSKpGi.png"
-                },
-                "author": {
-                  "name": "Bony SECURE WARNING",
-                  "icon_url": "https://i.imgur.com/3wSKpGi.png"
-                },
-              };
-              msg.channel.send('```Realtime scan disabled.```', {embed});
-              logCh.send({embed});
-              realtimeScanDisable = true;
-              return;
-            }
-            if(msg.content == "mode enable"){
-              client.user.setPresence({
-                status: "online",  //You can show online, idle....
-                activity: {
-                    name: "✔この鯖は保護されています",  //The message shown
-                    type: "LISTENING" //PLAYING: WATCHING: LISTENING: STREAMING:
-                }
-              });
-              msg.channel.send('```Realtime scan enabled.```')
-              realtimeScanDisable = false;
-              return;
-            }
-            msg.channel.send("```Command invalid.```");
-            return;
-        }
-        if(msg.content.indexOf("!sushi") !== -1) sushi(msg);
-        if(msg.content.indexOf("!member") !== -1) memberChecker(msg);
-        if(realtimeScanDisable){
-          return
-        }
-        if(msg.content.indexOf("!forceBlock") !== -1){
-          anl.cnt = 100;
-          msg.channel.send("強制的にブロック処理を行います。※試験的機能としてお使いください。");
-        }
-        if(anl.on){
-            anl.cnt++;
-        }else{
-            await startCheck(msg.channel, msg.guild);
-        }
-    }
+  console.log(JSON.stringify(msg.author));
+  if(msg.author != client.user){
+    if(msg.channel.id == ids.logCh) {msg.delete(); return;}
+    if(msg.channel.id == ids.terminalCh){
+      if(msg.content == "mode disable"){
+        client.user.setPresence({
+          status: "dnd",  //You can show online, idle....
+          activity: {
+              name: "☓この鯖は危険な状態です",  //The message shown
+              type: "LISTENING" //PLAYING: WATCHING: LISTENING: STREAMING:
+          }
+        });
+        const embed = {
+          "title": "**リアルタイムスキャンは無効です**",
+          "description": `サーバーを保護するには、今すぐリアルタイムスキャンを有効にしてください。`,
+          "color": 16711680,
+          "timestamp": new Date(),
+          "footer": {
+            "icon_url": "https://cdn.discordapp.com/avatars/709077937005264948/ebe1823c4fd5cd615d67915ba4c2d5a8.png",
+            "text": "Protected by Bony SECURITY POLICE"
+          },
+          "thumbnail": {
+            "url": "https://i.imgur.com/3wSKpGi.png"
+          },
+          "author": {
+            "name": "Bony SECURE WARNING",
+            "icon_url": "https://i.imgur.com/3wSKpGi.png"
+          },
+        };
+        msg.channel.send('```Realtime scan disabled.```', {embed});
+        logCh.send({embed});
+        realtimeScanDisable = true;
+        return;
+      }
+      if(msg.content == "mode enable"){
+        client.user.setPresence({
+          status: "online",  //You can show online, idle....
+          activity: {
+              name: "✔この鯖は保護されています",  //The message shown
+              type: "LISTENING" //PLAYING: WATCHING: LISTENING: STREAMING:
+          }
+        });
+        msg.channel.send('```Realtime scan enabled.```')
+        realtimeScanDisable = false;
+        return;
+      }
+      msg.channel.send("```Command invalid.```");
+      return;
+      }
+      if(msg.content.indexOf("!sushi") !== -1) sushi(msg);
+      if(msg.content.indexOf("!member") !== -1) memberChecker(msg);
+      if(realtimeScanDisable){
+        return
+      }
+      if(msg.content.indexOf("!forceBlock") !== -1){
+        anl.cnt = 100;
+        msg.channel.send("強制的にブロック処理を行います。※試験的機能としてお使いください。");
+      }
+      if(anl.on){
+          anl.cnt++;
+      }else{
+          await startCheck(msg.channel, msg.guild);
+      }
+  }
 });
 
 client.login(accessToken);
