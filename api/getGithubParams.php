435ce565f2453e7e5d9a5f739ea3a50488039c44
Bony_Chops <office.kentarou@gmail.com>
2020-05-18 13:00:58 +0900

    Edit

--- api/getGithubParams.php
+++ api/getGithubParams.php
@@ -1,2 +1,4 @@
 <?php
-file_put_contents(__DIR__.'/../github.json', json_encode($_POST, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+$json_string = file_get_contents('php://input');
+$json_object = json_decode($json_string);
+file_put_contents(__DIR__.'/../github.json', json_encode($json_object, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
